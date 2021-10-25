dfd72754d0c04143807572d6a8cee5a14b37ad9b
bhavya691 <bhavyarajdeora0@gmail.com>
2021-10-25 20:46:33 +0530

    c-99

--- Removefiles.py
+++ Removefiles.py
@@ -2,20 +2,22 @@ import os
 import shutil
 import time
 path = input('Enter the path: \t')
-days = input('Enter number of days: \t')
+days = int(input('Enter number of days: \t'))
 days = time.time()
+print(days)
 if os.path.exists(path):
     for root, subFols, files in os.walk(path):
         a = os.path.join(path,root)
-        print(a)
-        ctime = os.stat(a).st_ctime
+        # print(a)
+        ctime = os.stat(path).st_ctime
+        print(ctime)
         if ctime > days:
             if subFols:
-                print(subFols)
+                # print(subFols)
                 shutil.rmtree(a)
             elif files:
                 for file in files:
-                    print(file)
+                    # print(file)
                     os.remove(a,'\\',file)     
 else:
     print('Not found')
