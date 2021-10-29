88e04a2da034872a0bf5e3ffc1a6851546dbdd01
bhavya691 <bhavyarajdeora0@gmail.com>
2021-10-29 22:49:21 +0530

    working c-99

--- Removefiles.py
+++ Removefiles.py
@@ -1,23 +1,57 @@
 import os
 import shutil
 import time
-path = input('Enter the path: \t')
-days = int(input('Enter number of days: \t'))
-days = time.time()
-print(days)
-if os.path.exists(path):
-    for root, subFols, files in os.walk(path):
-        a = os.path.join(path,root)
-        # print(a)
-        ctime = os.stat(path).st_ctime
-        print(ctime)
-        if ctime > days:
-            if subFols:
-                # print(subFols)
-                shutil.rmtree(a)
-            elif files:
+
+
+def remove_folder(path):
+    if not shutil.rmtree(path):
+        print(f"{path} is removed successfully")
+    else:
+        print(f"Unable to delete the "+path)
+
+
+def remove_file(path):
+    if not os.remove(path):
+        print(f"{path} is removed successfully")
+    else:
+        print("Unable to delete the "+path)
+
+
+def get_file_or_folder_age(path):
+    ctime = os.stat(path).st_ctime
+    return ctime
+
+
+def main():
+    path = input('Enter the path: \t')
+    days = int(input('Enter the number of days: \t'))
+    seconds = time.time() - (days * 24 * 60 * 60)
+
+    if os.path.exists(path):
+
+        for root_folder, folders, files in os.walk(path):
+
+            if seconds >= get_file_or_folder_age(root_folder):
+                remove_folder(root_folder)
+                break
+            else:
+                for folder in folders:
+                    folder_path = os.path.join(root_folder, folder)
+                    if seconds >= get_file_or_folder_age(folder_path):
+                        remove_folder(folder_path)
                 for file in files:
-                    # print(file)
-                    os.remove(a,'\\',file)     
-else:
-    print('Not found')
+                    file_path = os.path.join(root_folder, file)
+                    if seconds >= get_file_or_folder_age(file_path):
+                        remove_file(file_path)
+        else:
+            if seconds >= get_file_or_folder_age(path):
+                remove_file(path)
+
+    else:
+        print(f'"{path}" is not found')
+
+    print(f"All Folders and Files are deleted")
+
+
+if __name__ == '__main__':
+    main()
